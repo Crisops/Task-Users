5e98b41d925f4a54ec71fb382035f990992bd62c
Crisops <crialeperez1835@gmail.com>
2024-07-12 17:10:08 -0500

    Create file app with routes and cookies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+
+import authRouter from './routes/auth.routes.js'
+import taskRouter from './routes/task.routes.js'
 
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api', authRouter)
+app.use('/api', taskRouter)
 
 export default app
