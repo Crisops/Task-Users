412a0ea8fdde9fce46ae21247ee862f0f3055ed6
Crisops <crialeperez1835@gmail.com>
2024-07-12 17:31:50 -0500

    Add file with functions get, create, update and delete task

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,51 @@
+import Task from '../models/task.model.js'
+
+export const tasks = async (req, res) => {
+  const { id } = req.session
+  const tasks = await Task.find({ user: id })
+  res.json(tasks)
+}
+
+export const getTask = async (req, res) => {
+  const { id } = req.params
+
+  const taskFind = await Task.findById(id)
+
+  if (!taskFind) return res.status(404).json({ error: 'Task not found' })
+  res.json(taskFind)
+}
+
+export const createTask = async (req, res) => {
+  console.log(req.body)
+  const { title, description, date } = req.body
+
+  const newTask = await new Task({
+    title,
+    description,
+    date,
+    user: req.session.id
+  })
+
+  const savedTask = await newTask.save()
+
+  res.json(savedTask)
+}
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params
+  const task = req.body
+
+  const updateTask = await Task.findByIdAndUpdate(id, task, { new: true })
+  if (!updateTask) return res.status(404).json({ error: 'Task not found' })
+
+  res.json(updateTask)
+}
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params
+
+  const taskFind = await Task.findByIdAndDelete(id)
+
+  if (!taskFind) return res.status(404).json({ error: 'Task not found' })
+  res.sendStatus(204)
+}
