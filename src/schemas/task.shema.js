bce9dac53b0c7fa0f082f97d61614bd8562b4e50
Crisops <crialeperez1835@gmail.com>
2024-07-12 17:29:30 -0500

    Schema task

---
+++ src/schemas/task.shema.js
@@ -0,0 +1,16 @@
+import z from 'zod'
+
+export const taskSchema = z.object({
+  title: z
+    .string({
+      required_error: 'Title is required',
+      invalid_type_error: 'Title must be a string'
+    }),
+  description: z
+    .string({
+      required_error: 'Description is required',
+      invalid_type_error: 'Description must be a string'
+    }),
+  date: z
+    .string().datetime().optional()
+})
