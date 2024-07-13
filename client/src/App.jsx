ec55ba9ab14454872e6ecfd5340c9e9670605590
Crisops <crialeperez1835@gmail.com>
2024-07-12 20:48:24 -0500

    Add components for render in path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Register from './components/Register'
+import Login from './components/Login'
 
 function App () {
   return (
     <section className='bg-zinc-900 w- full h-dvh'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<div>Home</div>} />
-          <Route path='/register' element={<div>Register</div>} />
-          <Route path='/login' element={<div>Login</div>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </section>
