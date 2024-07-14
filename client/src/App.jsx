63a90a60188210711069ded100b5d449e19355d4
Crisops <crialeperez1835@gmail.com>
2024-07-14 13:05:48 -0500

    Add Context Provider in the router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Register from './components/Register'
-import Login from './components/Login'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import { AuthProvider } from './context/AuthContext'
 
 function App () {
   return (
     <section className='bg-zinc-900 w- full h-dvh'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/tasks' element={<h1>Tasks</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </section>
   )
 }
