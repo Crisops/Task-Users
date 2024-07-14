cf6d78b7d3a70df9af45dc861db40173e4f4fe63
Crisops <crialeperez1835@gmail.com>
2024-07-14 10:40:22 -0500

    Agg function middleware cors

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import cookieParser from 'cookie-parser'
 
 import authRouter from './routes/auth.routes.js'
 import taskRouter from './routes/task.routes.js'
+import { middlewareCors } from './middlewares/middlewareCors.js'
 
 const app = express()
 
+app.use(middlewareCors())
 app.use(express.json())
 app.use(cookieParser())
 
