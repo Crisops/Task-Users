0c99c67c21ad22954f86f60b0b964683712d7bd5
Crisops <crialeperez1835@gmail.com>
2024-07-15 17:39:26 -0500

    Agg function verifyToken in router

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { register, login, logout, profile } from '../controllers/auth.controller.js'
+import { register, login, logout, profile, verifyToken } from '../controllers/auth.controller.js'
 import { authRequired } from '../middlewares/validateToken.js'
 import { validationShema } from '../middlewares/validator.middleware.js'
 import { registerSchema, loginSchema } from '../schemas/auth.schema.js'
@@ -10,5 +10,6 @@ router.post('/register', validationShema(registerSchema), register)
 router.post('/login', validationShema(loginSchema), login)
 router.post('/logout', logout)
 router.get('/profile', authRequired, profile)
+router.get('/verify', authRequired, verifyToken)
 
 export default router
