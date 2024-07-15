7b6dfd11edd2ecd4b8a61926a45ea06a07e989da
Crisops <crialeperez1835@gmail.com>
2024-07-15 17:30:10 -0500

    Agg function verifytoken for checking if the token existed and user have access

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -15,3 +15,20 @@ export function fetchAuth (url, data) {
       .catch(error => reject(error))
   })
 }
+
+export function verifyToken (url) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then(user => resolve(user))
+      .catch(error => reject(error))
+  })
+}
