1c2618909cc65ae4ab1bf67ee95fbe6017638080
Crisops <crialeperez1835@gmail.com>
2024-07-15 12:15:19 -0500

    Add a function signin with an effect that sets the initial error value after 5 seconds.

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { fetchAuth } from '../api/auth'
 import { URL_API } from '../../config'
 
@@ -8,6 +8,14 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setError(null)
+    }, 5000)
+
+    return () => clearTimeout(timeout)
+  }, [error])
 
   const signup = async (user) => {
     try {
@@ -20,7 +28,18 @@ export const AuthProvider = ({ children }) => {
     }
   }
 
+  const signin = async (user) => {
+    try {
+      const res = await fetchAuth(`${URL_API}/login`, user)
+      if (res.error) throw res.error
+      setUser(res)
+      setIsAuthenticated(true)
+    } catch (error) {
+      setError(error)
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ signup, user, isAuthenticated, error }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signup, signin, user, isAuthenticated, error }}>{children}</AuthContext.Provider>
   )
 }
