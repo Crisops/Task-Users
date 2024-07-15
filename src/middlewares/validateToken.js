77ecca18b6beaaf6f8510d63ef611a6f2f522645
Crisops <crialeperez1835@gmail.com>
2024-07-15 17:33:43 -0500

    Agg return if there is no token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,7 +3,7 @@ import { JWT_SECRET } from '../config.js'
 
 export const authRequired = (req, res, next) => {
   const token = req.cookies.access_token
-  if (!token) res.status(401).json({ message: 'Not Authorized' })
+  if (!token) return res.status(401).json({ message: 'Not Authorized' })
 
   jsw.verify(token, JWT_SECRET, (err, user) => {
     if (err) return res.status(401).json({ message: 'Invalid Token' })
