81af439878a44d25d4e53bdbfebf7a74cf3942fd
Crisops <crialeperez1835@gmail.com>
2024-07-17 12:10:02 -0500

    Add file CreateTask

---
+++ client/src/pages/CreateTask.jsx
@@ -0,0 +1,11 @@
+import CreateTaskForm from '../components/CreateTaskForm'
+
+function CreateTask () {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <CreateTaskForm />
+    </div>
+  )
+}
+
+export default CreateTask
